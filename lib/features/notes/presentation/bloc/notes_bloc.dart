90bd6dcfdfdc8ae42f3c4e50d275bcd9d7dd34f7
MarwanGhareeb <marwanghareeb146@gmail.com>
2025-10-16 03:29:31 +0300

    init bloc files

---
+++ lib/features/notes/presentation/bloc/notes_bloc.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'notes_event.dart';
+part 'notes_state.dart';
+
+class NotesBloc extends Bloc<NotesEvent, NotesState> {
+  NotesBloc() : super(NotesInitial());
+}
